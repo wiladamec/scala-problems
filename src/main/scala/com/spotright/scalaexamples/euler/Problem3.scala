cbdfadb4a6405aa478e77867475750259f67c9ba
Wil Adamec <wil@spotright.com>
2015-06-24 16:21:16 -0500

    Removed extra code from Problem 3

--- src/main/scala/com/spotright/scalaexamples/euler/Problem3.scala
+++ src/main/scala/com/spotright/scalaexamples/euler/Problem3.scala
@@ -4,21 +4,6 @@ import scala.collection.mutable.ArrayBuffer
 
 object Problem3 {
 
-  // TODO rewrite this to be faster
-  def largestPrimeFactor1(n: Long): Long = {
-    var i = 2
-    var j = n
-
-    while(i * i < n) {
-      while(j % i == 0)
-        j = j / i
-
-      i += 1
-    }
-
-    j
-  }
-
   def primeFactors(n: Long): List[Long] = {
     (2 to math.sqrt(n).toInt).find(n % _ == 0).fold(List(n))(i => i.toLong :: primeFactors(n / i))
   }
