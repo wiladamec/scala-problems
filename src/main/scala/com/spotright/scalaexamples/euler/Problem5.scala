76fcb65347e38088221cf289f61c9327fceaa132
Wil Adamec <wil@spotright.com>
2015-06-25 14:43:07 -0500

    Added notes to problem 5

--- src/main/scala/com/spotright/scalaexamples/euler/Problem5.scala
+++ src/main/scala/com/spotright/scalaexamples/euler/Problem5.scala
@@ -4,7 +4,22 @@ object Problem5 {
 
   def main(av: Array[String]): Unit = {
 
-    val lcm = Iterator.range(2520, Int.MaxValue, 2520).find(n => (11 to 20).forall(n % _ == 0)).get
+    /**
+     * Some notes so I remember what I did here...
+     *
+     * Since 2520 is divisible by 1 ... 10, we know that:
+     *
+     * 1. Any number divisible by 1 ... 10 is a multiple of 2520
+     * 2. Any multiple of 2520 will be divisible by 1 ... 10
+     *
+     * This significantly reduces the number of checks we have to do. We can check
+     * only multiples of 2520 starting with 2520*2, and check for divisibility by
+     * only 11 ... 20.
+     *
+     * It's brute forcing, but with care. :)
+     */
+
+    val lcm = Iterator.range(5040, Int.MaxValue, 2520).find(n => (11 to 20).forall(n % _ == 0)).get
 
     println(lcm)
   }
