a605e1e6bdaf31b960a28dc839bd23f348956e10
Wil Adamec <wil@spotright.com>
2015-06-29 09:41:21 -0500

    Added initial HWQueue implementation

--- src/main/scala/com/spotright/scalaexamples/datastructures/HighwaterQueue.scala
+++ src/main/scala/com/spotright/scalaexamples/datastructures/HighwaterQueue.scala
@@ -1,5 +1,44 @@
 package com.spotright.scalaexamples.datastructures
 
+import scala.collection.mutable.ArrayBuffer
+
 class HighwaterQueue[A](hwSize: Int, func: A => Unit) {
 
+  private val queue: ArrayBuffer[A] = ArrayBuffer[A]()
+  var length: Int = 0
+
+  def isEmpty: Boolean = {
+
+    this.length == 0
+  }
+
+  def enqueue(elem: A): Unit = {
+
+    this.queue.append(elem)
+    this.length += 1
+
+    if(this.length == hwSize) this.queue.foreach(func(_))
+  }
+
+  def dequeue: Option[A] = {
+
+    if(!this.isEmpty) {
+      val elem = queue.head
+      this.queue.remove(0)
+      this.length -= 1
+
+      Some(elem)
+    } else {
+      None
+    }
+  }
+
+  def peek: Option[A] = {
+
+    if(!this.isEmpty) {
+      Some(this.queue.head)
+    } else {
+      None
+    }
+  }
 }
