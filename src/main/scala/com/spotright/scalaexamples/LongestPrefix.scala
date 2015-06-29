26b20e616d4758b24d17049d5ab2951dcf790c14
Wil Adamec <wil@spotright.com>
2015-06-29 08:47:01 -0500

    LongestPrefix condition change

--- src/main/scala/com/spotright/scalaexamples/LongestPrefix.scala
+++ src/main/scala/com/spotright/scalaexamples/LongestPrefix.scala
@@ -6,19 +6,13 @@ object LongestPrefix {
 
   @tailrec def longestPrefix(strings: List[String])(implicit prefix: String = strings.head): Int = {
 
-    if(prefix.isEmpty) -1
-    else if(strings.map(_.take(prefix.length)).count(_ == prefix) == strings.length) prefix.length
+    if(strings.map(_.take(prefix.length)).count(_ == prefix) == strings.length) prefix.length
     else longestPrefix(strings)(prefix.substring(0, prefix.length - 1))
   }
 
   def main(av: Array[String]): Unit = {
 
     val strings = List("runway", "runway24", "runaway")
-    val prefixLength: Int = longestPrefix(strings)
-
-    prefixLength match {
-      case -1 => println("There is no common prefix among these strings")
-      case _  => println("The length of the longest common prefix is " + prefixLength)
-    }
+    println(s"The length of the longest common prefix is ${longestPrefix(strings)}")
   }
 }
